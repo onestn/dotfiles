f88040c6183c3a62b86edefd32a4fdba8644d902
dead-1ine <yang991211@naver.com>
2023-12-13 15:58:11 +0900

    chore: Remove wrong closed brackets

--- nvim/lua/dead-line/plugins/nvim-cmp.lua
+++ nvim/lua/dead-line/plugins/nvim-cmp.lua
@@ -38,19 +38,18 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       -- sources for autocompletion
-      sources = cmp.config.sources({
+
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
-      }),
+      })
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
         }),
-      },
-    })
+      }
   end,
 }
