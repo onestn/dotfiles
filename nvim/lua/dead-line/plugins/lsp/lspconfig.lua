4dff4e38d246fb887ae4bad2ed22fd10e3a72917
dead-1ine <yang991211@naver.com>
2023-12-17 09:19:40 +0900

    feat: Remove java-language-server in mason

--- nvim/lua/dead-line/plugins/lsp/lspconfig.lua
+++ nvim/lua/dead-line/plugins/lsp/lspconfig.lua
@@ -67,6 +67,7 @@ return {
       Hint = "󰠠 ",
       Info = " "
     }
+
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- nvim/lua/dead-line/plugins/lsp/mason.lua
+++ nvim/lua/dead-line/plugins/lsp/mason.lua
@@ -31,7 +31,6 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
