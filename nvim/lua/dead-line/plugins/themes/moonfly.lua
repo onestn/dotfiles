48222bd440e196e28f05d404f1a6d33f97180f5b
dead-1ine <yang991211@naver.com>
2023-12-23 23:50:04 +0900

    chore: Delete lazy = false on theme files

--- nvim/lua/dead-line/plugins/themes/moonfly.lua
+++ nvim/lua/dead-line/plugins/themes/moonfly.lua
@@ -1,6 +1,5 @@
 return {
   "bluz71/vim-moonfly-colors",
   name = "moonfly",
-  lazy = false,
   priority = 1000
 }
--- nvim/lua/dead-line/plugins/themes/onedark.lua
+++ nvim/lua/dead-line/plugins/themes/onedark.lua
@@ -1,6 +1,5 @@
 return {
   "navarasu/onedark.nvim",
   name = "onedark",
-  lazy = false,
   priority = 1000
 }
