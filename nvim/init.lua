f5557ed5d8b00c9f185ba34779178001337cdf01
dead-1ine <yang991211@naver.com>
2023-12-25 10:40:27 +0900

    feat: Delete ftplugin/

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+USER_PROFILE = "dead-line"
+
 -- import .vimrc
 vim.cmd("source ~/.vimrc")
 
@@ -24,8 +26,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
+-- Java
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
 
 -- import settings
 -- TODO: Why does <leader> not work to change top line to declare
-require("dead-line.lazy")
-require("dead-line.mappings")
+require(USER_PROFILE .. ".lazy")
+require(USER_PROFILE .. ".mappings")
--- nvim/lua/dead-line/ftplugin/java.lua
+++
@@ -1,4 +0,0 @@
-vim.bo.tabstop = 4 -- size of a had tabstop
-vim.bo.shiftwidth = 4 -- size of an indentation
-vim.bo.expandtab = true -- always uses spaces instead of tab characters
-vim.bo.softtabstop = 4 -- number of spaces a <Tab> counts for. 
