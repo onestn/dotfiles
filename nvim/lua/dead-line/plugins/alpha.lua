55fd8245b27b4abfb8afe61e6ade55f417954b84
dead-1ine <yang991211@naver.com>
2023-12-27 21:10:07 +0900

    chore: Add opt in dressing & Update lazy-lock with automation & Update not primary things

---
+++ nvim/lua/dead-line/plugins/alpha.lua
@@ -0,0 +1,52 @@
+return {
+  "goolord/alpha-nvim",
+  config = function ()
+    local alpha = require'alpha'
+    local dashboard = require'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      [[                                                    ]],
+      [[                                                    ]],
+      [[                                                    ]],
+      [[                                                    ]],
+      [[                                                    ]],
+      [[                                                    ]],
+      [[          ██  ████████  ██                          ]],
+      [[          ████████████████                          ]],
+      [[          ██████████████████                        ]],
+      [[        ████  ████  ██████████                      ]],
+      [[        ████████████████████████                    ]],
+      [[        ██████    ██████████████████   dead-1ine    ]],
+      [[        ██  ██  ████  ████████████████████████████  ]],
+      [[        ████        ████████████████████████        ]],
+      [[        ████████████████████████████████████        ]],
+      [[        ████████████████████████████████████        ]],
+      [[        ████████████████████████████████████        ]],
+      [[        ████████████████████████████████████        ]],
+      [[        ██████████████████████████████████          ]],
+      [[          ████████████████████████████████          ]],
+      [[          ████    ████        ████    ████          ]],
+      [[          ████    ████        ████    ████          ]],
+      [[          ██      ██          ██      ██            ]],
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "1  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("e", "2  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("p", "3  Find project", ":Telescope projects <CR>"),
+      dashboard.button("r", "4  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", "5  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "6  Configuration", ":e $MYVIMRC<CR>"),
+      dashboard.button("q", "7  Quit Neovim", ":qa<CR>"),
+    }
+    local handle = io.popen('fortune')
+    local fortune = handle:read("*a")
+    handle:close()
+    dashboard.section.footer.val = fortune
+
+    dashboard.config.opts.noautocmd = true
+
+    vim.cmd[[autocmd User AlphaReady echo 'ready']]
+
+    alpha.setup(dashboard.config)
+  end
+}
