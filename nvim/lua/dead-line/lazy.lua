ab25f9fc6c6b23ddfd4756edd77d5d21ebd5b566
wonseokyang-de <deadline.yang@gmail.com>
2025-03-03 11:23:44 +0900

    feat: Change theme to catpuccin

--- nvim/lua/dead-line/lazy.lua
+++ nvim/lua/dead-line/lazy.lua
@@ -10,6 +10,6 @@ require("lazy").setup({
   change_detection = { notify = false },
 })
 
--- vim.cmd.colorscheme "catppuccin-mocha"
-vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd[[colorscheme catppuccin-mocha]]
+-- vim.cmd[[colorscheme tokyonight-night]]
 -- vim.cmd[[colorscheme tokyodark]]
