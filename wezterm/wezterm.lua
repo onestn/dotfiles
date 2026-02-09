a526aa3b85af8d6cae4936e4e50a9cfe3c073752
onestn <onestone.yang@gmail.com>
2026-02-09 20:47:32 +0900

    chore: Add some ignore rules and some wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,27 @@
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder and wezterm.config_builder() or {}
 
-return {
-  font = wezterm.font_with_fallback({
-    "JetBrains Mono",
-    "MesloLGS NF", -- Nerd Font fallback
-  }),
-  font_size = 14.0,
-
-  color_scheme = "Catppuccin Mocha",
-
-  enable_tab_bar = false,
-  window_decorations = "RESIZE",
-  window_padding = {
-    left = 6, right = 6, top = 6, bottom = 6,
-  },
-  adjust_window_size_when_changing_font_size = false,
-
-  macos_window_background_blur = 20,
-  window_background_opacity = 0.92,
+config.font = wezterm.font_with_fallback({
+  "JetBrains Mono",
+  "MesloLGS NF", -- Nerd Font fallback
+})
+config.font_size = 14.0
+config.color_scheme = "Catppuccin Mocha"
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 6, right = 6, top = 6, bottom = 6,
 }
+config.adjust_window_size_when_changing_font_size = false
+config.window_background_opacity = 0.92
+
+if wezterm.target_os == "Windows" then
+  config.default_prog = {
+    "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe",
+    "-NoLogo",
+  }
+elseif wezterm.target_os == "Darwin" then
+  config.macos_window_background_blur = 20
+end
+
+return config
