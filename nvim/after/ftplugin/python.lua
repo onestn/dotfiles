4a974faa774d9532f60bd4ac01a55e2a5f8b983b
onestn <onestone.yang@gmail.com>
2026-02-13 14:10:56 +0900

    chore: clean python format setting

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -3,5 +3,4 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.commentstring = "# %s"
 
-vim.opt_local.textwidth = 79
-vim.opt_local.smartindent = true
+vim.opt_local.textwidth = 0
